3036aeb729d9886deac0937f0c1fea5cda7f4bb0
Paweł Czekaj <paw.czekaj@gmail.com>
2024-05-23 18:03:44 +0200

    added Dataset type

--- Fetch/fetch.ts
+++ Fetch/fetch.ts
@@ -2,9 +2,11 @@ import {RawData} from "../Interfaces/Candle";
 import * as fs from "fs";
 import * as path from "path";
 import {DATA_BASE_PATH} from "../Constants/paths";
+import {Dataset} from "../Interfaces/Other";
 
 const fetchData = function (symbol: string, interval: string): RawData {
     return JSON.parse(fs.readFileSync(path.join(DATA_BASE_PATH, symbol, interval, 'data.json')).toString());
 }
+
 
 export {fetchData}
