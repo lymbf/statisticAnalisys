907120e3f561191bfc89219da0b80a0c4bb2aca9
Paweł Czekaj <paw.czekaj@gmail.com>
2024-05-23 18:07:13 +0200

    changed: constants: datasets path

--- Constants/paths.ts
+++ Constants/paths.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 
 
 const DATA_BASE_PATH: string = path.join(__dirname, '../../', 'DataColector', 'Data');
-const DATASETS_PATH: string = path.join(__dirname, '../../', 'DataTester', 'DataSets')
+const DATASETS_PATH: string = path.join(__dirname, '../', 'DataSets')
 
 
 export {DATASETS_PATH, DATA_BASE_PATH}
