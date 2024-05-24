b6b2edbd4d36384d0887b60d35076038f2fb7c6c
Paweł Czekaj <paw.czekaj@gmail.com>
2024-05-24 21:20:38 +0200

    type: TradeDirecction 1/-1

--- Main/TestingBots/MoonCycles/fullMoonCRCycles.ts
+++ Main/TestingBots/MoonCycles/fullMoonCRCycles.ts
@@ -1,12 +1,12 @@
 import {fetchDataset} from "../../../Fetch/fetch";
 import {Candle, Timestamp} from "../../../Interfaces/Candle";
 import {getCumulativeReturnsMap} from "../../Libs/ChangeAnalisys/CumulativeReturns/cumulativeReturns";
+import {TradeDirection} from "../../../Interfaces/Trades";
 
 
 const fullMoonDates: Timestamp[] = fetchDataset('fullMoonDates');
 
 const performTrade = function (signal: Timestamp, data: Candle[]) {
-    
 
 }
 
