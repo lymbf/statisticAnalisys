e1e01d037d935bb94c7dd333cb38a485cbb64e81
Paweł Czekaj <paw.czekaj@gmail.com>
2024-05-24 21:14:14 +0200

    interfaces: Trades -> single trade result and setup result

--- Main/TestingBots/MoonCycles/fullMoonCRCycles.ts
+++ Main/TestingBots/MoonCycles/fullMoonCRCycles.ts
@@ -1,10 +1,12 @@
 import {fetchDataset} from "../../../Fetch/fetch";
 import {Candle, Timestamp} from "../../../Interfaces/Candle";
+import {getCumulativeReturnsMap} from "../../Libs/ChangeAnalisys/CumulativeReturns/cumulativeReturns";
 
 
 const fullMoonDates: Timestamp[] = fetchDataset('fullMoonDates');
 
 const performTrade = function (signal: Timestamp, data: Candle[]) {
+    
 
 }
 
